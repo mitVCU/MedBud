a3edced2100cd84e7d1a2d6456e672998766ef8c
sathishb123 <31788083+sathishb123@users.noreply.github.com>
2017-09-09 02:36:05 -0400

    updated with calendar input.

--- app/src/main/java/com/mittens/medbud/MainActivity.java
+++ app/src/main/java/com/mittens/medbud/MainActivity.java
@@ -1,14 +1,36 @@
 package com.mittens.medbud;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static com.mittens.medbud.R.id.btnGoCalendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private TextView theDate;
+    private Button btnGoCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        theDate = ( TextView ) findViewById(R.id.date);
+        btnGoCalendar = ( Button ) findViewById(R.id.btnGoCalendar );
+
+        btnGoCalendar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
 
